540fc609bb443045d589183cb2d5eb7115c29915
Shubham Mishra <sh277446@dal.ca>
2023-05-24 20:35:35 -0300

    A1: send error codes

--- A1/flask1/main.py
+++ A1/flask1/main.py
@@ -8,15 +8,14 @@ app = Flask(__name__)
 @app.route('/calculate', methods=['POST'])
 def calculate():
     input_data = request.json
-    if "file" in input_data and input_data["file"] is not None:
+    if "file" in input_data and "product" in input_data and input_data["file"] is not None and input_data["product"] is not None:
         file = input_data["file"]
         if os.path.isfile("host_vol/"+file):
             response = requests.post("http://flask2:6003/calculate", json=input_data)
-            print(response.json())
             return response.json()
-        return {"file": file, "error": "File not found."}
+        return {"file": file, "error": "File not found."}, 404
 
-    return {"file": None, "error": "Invalid JSON input."}
+    return {"file": None, "error": "Invalid JSON input."}, 404
 
 
 if __name__ == '__main__':
--- A1/flask2/main.py
+++ A1/flask2/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/calculate', methods=['POST'])
 def calculate():
     input_data = request.json
-    if "file" in input_data and input_data["file"] is not None:
+    if "file" in input_data and "product" in input_data and input_data["file"] is not None and input_data["product"] is not None:
         file = input_data["file"]
         product = input_data["product"]
         file_path = "host_vol/"+file
@@ -25,9 +25,9 @@ def calculate():
             else:
                 val = required_product.iloc[0]["amount"]
             return {"file": file, "sum": int(val)}
-        return {"file": file, "error": "File not found."}
+        return {"file": file, "error": "File not found."}, 404
 
-    return {"file": None, "error": "Invalid JSON input."}
+    return {"file": None, "error": "Invalid JSON input."}, 404
 
 
 if __name__ == '__main__':
